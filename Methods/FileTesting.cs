319cf61ceff62637cbbd18eb5608a077868fadc6
AddyMills <74471839+AddyMills@users.noreply.github.com>
2023-11-19 14:22:18 -0600

    Add Testing Method

---
+++ Methods/FileTesting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GH_Toolkit_Core.Methods
+{
+    public class FileTesting
+    {
+        public Action<string> TestFunc { get; set; }
+        public FileTesting(Action<string> testFunc)
+        {
+            TestFunc = testFunc;
+        }
+        public void TestProgram(string filePath)
+        {
+            List<string> files = new List<string>();
+            if (Directory.Exists(filePath))
+            {
+                foreach (string file in Directory.GetFiles(filePath)) { files.Add(file); }
+            }
+            else if (File.Exists(filePath))
+            {
+                files.Add(filePath);
+            }
+            else
+            {
+                throw new Exception("Could not find valid file or folder to parse.");
+            }
+            foreach (string file in files)
+            {
+                TestFunc(file);
+            }
+        }
+    }
+}
