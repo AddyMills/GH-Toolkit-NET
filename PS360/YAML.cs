15e2193ab5264c32976fe99e9afe722f6e6e81fd
AddyMills <74471839+AddyMills@users.noreply.github.com>
2024-04-25 22:50:16 -0600

    Add YAML code and thumbnails for Onyx packaging

---
+++ PS360/YAML.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+using YamlDotNet.Serialization;
+
+namespace GH_Toolkit_Core.PS360
+{
+    public class YAML
+    {
+        private static string ExeLocation = Assembly.GetExecutingAssembly().Location;
+        private static string ExeDirectory = Path.GetDirectoryName(ExeLocation);
+
+        private static int gh3Title = 0x415607F7;
+        private static int ghwtTitle = 0x4156081A;
+        private static int gh5Title = 0x41560840;
+        private static int ghworTitle = 0x41560883;
+
+        private static Dictionary<string, int> gameTitles = new Dictionary<string, int>
+        {
+            { "GH3", gh3Title },
+            { "GHWT", ghwtTitle },
+            { "GH5", gh5Title },
+            { "GHWOR", ghworTitle }
+        };
+        private static Dictionary<string, string> gameNames = new Dictionary<string, string>
+        {
+            { "GH3", "Guitar Hero 3" },
+            { "GHWT", "Guitar Hero World Tour" },
+            { "GH5", "Guitar Hero 5" },
+            { "GHWOR", "Guitar Hero : Warriors of Rock" }
+        };
+        public static List<string> MakePackageDescription(string description = "Compiled by Addy's .NET Toolkit")
+        {
+            string[] list = [description, "", "", "", "", "", "", "", ""];
+            return list.ToList();
+        }
+        public static List<string> MakePackageName(string packageName = "")
+        {
+            string[] list = [packageName, "", "", "", "", "", "", "", ""];
+            return list.ToList();
+        }
+        public static string CreateOnyxYaml(string game = "GH3", string packageName = "")
+        {
+            string yamlLocation = Path.Combine(ExeDirectory, "Resources", "repack-stfs.yaml");
+            if (!File.Exists(yamlLocation))
+            {
+                Console.WriteLine("YAML file not found.");
+                return "Fail";
+            }
+            string yamlContent = File.ReadAllText(yamlLocation);
+            var deserializer = new DeserializerBuilder().Build();
+            var yamlObject = deserializer.Deserialize<Dictionary<string, object>>(yamlContent);
+            yamlObject["package-description"] = MakePackageDescription();
+            yamlObject["package-name"] = MakePackageName(packageName);
+            yamlObject["title-id"] = gameTitles[game];
+            yamlObject["title-name"] = gameNames[game];
+            var serializer = new SerializerBuilder().Build();
+            string yamlOutput = serializer.Serialize(yamlObject).Replace("\"\"","''");
+            return yamlOutput;
+        }
+    }
+}
