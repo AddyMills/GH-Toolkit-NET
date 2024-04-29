5ab3211ddf494b0d79081bb4e579340169685aea
AddyMills <74471839+AddyMills@users.noreply.github.com>
2024-04-28 20:19:02 -0600

    Give credit to mtolly

--- Methods/GHTCP.cs
+++ Methods/GHTCP.cs
@@ -8,6 +8,9 @@ namespace GH_Toolkit_Core.Methods
 {
     public class GHTCP
     {
+        // The private methods are ported from Onyx Toolkit by Michael Tolly (who in turn ported it from C# to Haskell)
+        // It's a perfect loop!
+
         private static readonly byte[] sghSetlistKey = [ 61, 250, 11, 73, 254, 154, 8, 191, 18, 188, 243, 32, 246, 40, 148, 145, 62, 219, 250, 196, 15, 63, 217, 91, 29, 73, 8, 22, 197, 186, 176, 81 ];
         private static readonly byte[] sghSetlistIV = [ 18, 188, 243, 32, 246, 40, 148, 145, 247, 74, 25, 30, 94, 26, 230, 111 ];
         private static readonly byte[] sghSongsKey = [ 45, 219, 244, 185, 119, 192, 19, 251, 134, 93, 62, 50, 245, 33, 177, 178, 192, 184, 16, 30, 114, 253, 61, 49, 248, 198, 204, 123, 91, 48, 188, 103 ];
@@ -29,7 +32,7 @@ namespace GH_Toolkit_Core.Methods
                 aesAlg.Key = key;
                 aesAlg.IV = iv;
                 aesAlg.Mode = CipherMode.CBC;
-                aesAlg.Padding = PaddingMode.PKCS7; // Default padding mode, can be changed if needed
+                aesAlg.Padding = PaddingMode.PKCS7; 
 
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
