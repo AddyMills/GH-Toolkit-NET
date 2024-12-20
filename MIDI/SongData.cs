9ebb1b6985dcdaefa0444ed735d05228e1b3e897
AddyMills <74471839+AddyMills@users.noreply.github.com>
2024-12-20 11:03:05 -0600

    CLI song compiling preparation

---
+++ MIDI/SongData.cs
@@ -0,0 +1,35 @@
+﻿using GH_Toolkit_Core.INI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GH_Toolkit_Core.INI.FileAssignment;
+using static GH_Toolkit_Core.INI.SongIniData;
+
+
+namespace GH_Toolkit_Core.MIDI
+{
+    public class SongData
+    {
+        private FileAssignment? filePathInfo = new FileAssignment();
+        private SongIniData? songInfo = new SongIniData();
+
+        public void SetSongInfo(SongIniData songInfo)
+        {
+            this.songInfo = songInfo;
+        }
+        public SongIniData GetSongInfo() {
+            return songInfo;
+        }
+        public void SetFilePathInfo(FileAssignment filePathInfo)
+        {
+            this.filePathInfo = filePathInfo;
+        }
+        public FileAssignment GetFilePathInfo()
+        {
+            return filePathInfo;
+        }
+
+    }
+}
