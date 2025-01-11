38f32128261f659e8fc52779ec94bea2e80ccede
AddyMills <74471839+AddyMills@users.noreply.github.com>
2025-01-10 21:01:42 -0600

    Add more global variables

--- Methods/GlobalVariables.cs
+++ Methods/GlobalVariables.cs
@@ -9,15 +9,21 @@ namespace GH_Toolkit_Core.Methods
     public class GlobalVariables
     {
         public static string ExeRootFolder { get; private set; }
+        public static string BlankWav { get; private set; }
         
         static GlobalVariables()
         {
             ExeRootFolder = GetRootFolder();
+            BlankWav = GetBlankWav();
         }
 
         static string GetRootFolder()
         {
             return AppContext.BaseDirectory;
+        }
+        static string GetBlankWav()
+        {
+            return Path.Combine(ExeRootFolder, "Audio\\Blank Audio\\blankWav.wav");
         }
     }
 }
