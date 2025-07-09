6c5366670b33f18f95a876ae676211c51f7a846a
AddyMills <74471839+AddyMills@users.noreply.github.com>
2025-07-08 21:08:42 -0600

    Add GlobalHelpers.cs

---
+++ Methods/GlobalHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GH_Toolkit_Core.Methods
+{
+    public class GlobalHelpers
+    {
+        public static string GetConsoleExtension(string console)
+        {
+            string extension = "";
+
+            switch (console)
+            {
+                case "PS2":
+                    extension = ".ps2";
+                    break;
+                case "PS3":
+                    extension = ".PS3";
+                    break;
+                case "WII":
+                    extension = ".ngc";
+                    break;
+                default:
+                    extension = ".xen";
+                    break;
+            }
+            return extension;
+        }
+    }
+}
