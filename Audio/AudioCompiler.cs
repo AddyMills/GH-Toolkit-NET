8f250f9c5857b154c7e4b53971c8b633664ae4c5
AddyMills <74471839+AddyMills@users.noreply.github.com>
2025-08-02 11:46:46 -0600

    GHWT audio compiling

--- Audio/AudioCompiler.cs
+++ Audio/AudioCompiler.cs
@@ -1,25 +1,21 @@
-﻿using ICSharpCode.SharpZipLib.Checksum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static GH_Toolkit_Core.QB.QBConstants;
+﻿using static GH_Toolkit_Core.QB.QBConstants;
 
 namespace GH_Toolkit_Core.Audio
 {
     public class AudioCompiler
     {
         // Global Settings
-        private string Checksum {  get; set; }
+        private string Checksum { get; set; }
         private string CompilePath { get; set; }
-        private string SavePath { get
+        private string SavePath
+        {
+            get
             {
                 return Path.Combine(CompilePath, SaveFolderName);
             }
         } // Just so it doesn't save everything in the same folder.
         private string SaveFolderName { get; set; }
-        private string CurrentGame {  get; set; }
+        private string CurrentGame { get; set; }
         private decimal PreviewStart { get; set; }
         private decimal PreviewLength { get; set; }
         private decimal PreviewVolume { get; set; }
@@ -44,61 +40,23 @@ namespace GH_Toolkit_Core.Audio
         private string GuitarCoopPath { get; set; }
         private string RhythmCoopPath { get; set; }
         private string[] BackingCoopPaths { get; set; }
-        private string FsbOut {  get; set; }
+        private string FsbOut { get; set; }
+        private string[] FsbList { get; set; }
         private string DatOut { get; set; }
-        public AudioCompiler() 
-        {  
+        public AudioCompiler()
+        {
         }
         public (string fsbout, string datout) getFsbDat()
         {
             return (FsbOut, DatOut);
         }
-        public static AudioCompiler CreateGh3Compiler(
-            string checksum,
-            string compilePath,
-            string saveFolder,
-            string game,
-            int previewStart,
-            int previewLength,
-            decimal previewVolume,
-            decimal fadeIn,
-            decimal fadeOut,
-            bool customPreview,
-            bool coopAudio,
-            string guitarPath,
-            string rhythmPath,
-            string[] backingPaths,
-            string crowdPath,
-            string previewPath,
-            string guitarCoopPath,
-            string rhythmCoopPath,
-            string[] backingCoopPaths)
+        public string[] getFsbList()
         {
-            decimal prevStart = previewStart / 1000m;
-            decimal prevLength = previewLength / 1000m;
-            var compiler = new AudioCompiler()
+            if (FsbList == null)
             {
-                Checksum = checksum,
-                CompilePath = compilePath,
-                SaveFolderName = saveFolder,
-                CurrentGame = game,
-                PreviewStart = prevStart,
-                PreviewLength = prevLength,
-                PreviewVolume = previewVolume,
-                FadeIn = fadeIn,
-                FadeOut = fadeOut,
-                CustomPreview = customPreview,
-                CoopAudio = coopAudio,
-                GuitarPath = guitarPath,
-                RhythmPath = rhythmPath,
-                BackingPaths = backingPaths,
-                CrowdPath = crowdPath,
-                PreviewPath = previewPath,
-                GuitarCoopPath = guitarCoopPath,
-                RhythmCoopPath = rhythmCoopPath,
-                BackingCoopPaths = backingCoopPaths,
-            };
-            return compiler;
+                FsbList = Array.Empty<string>();
+            }
+            return FsbList;
         }
         public async Task GH3AudioCompile()
         {
@@ -191,6 +149,186 @@ namespace GH_Toolkit_Core.Audio
             await msv.CreatePs2Preview(previewSave);
 
             await audioTask;
+        }
+        public async Task GHWTAudioCompile(bool encrypt = false)
+        {
+            string drumsKickOutput = Path.Combine(CompilePath, $"{Checksum}_drumsKick.mp3");
+            string drumsSnareOutput = Path.Combine(CompilePath, $"{Checksum}_drumsSnare.mp3");
+            string drumsTomsOutput = Path.Combine(CompilePath, $"{Checksum}_drumsToms.mp3");
+            string drumsCymbalOutput = Path.Combine(CompilePath, $"{Checksum}_drumsCymbal.mp3");
+
+            string guitarOutput = Path.Combine(CompilePath, $"{Checksum}_guitar.mp3");
+            string rhythmOutput = Path.Combine(CompilePath, $"{Checksum}_rhythm.mp3");
+            string vocalsOutput = Path.Combine(CompilePath, $"{Checksum}_vocals.mp3");
+
+            string crowdOutput = Path.Combine(CompilePath, $"{Checksum}_crowd.mp3");
+            string backingOutput = Path.Combine(CompilePath, $"{Checksum}_song.mp3");
+
+            string previewOutput = Path.Combine(CompilePath, $"{Checksum}_preview.mp3");
+
+            string[] drumFiles = { drumsKickOutput, drumsSnareOutput, drumsTomsOutput, drumsCymbalOutput };
+            string[] otherFiles = { guitarOutput, rhythmOutput, vocalsOutput };
+            string[] backingFiles = { backingOutput, crowdOutput };
+
+            var filesToProcess = new List<string>();
+            filesToProcess.AddRange(drumFiles);
+            filesToProcess.AddRange(otherFiles);
+            filesToProcess.AddRange(backingFiles);
+            filesToProcess.Add(previewOutput);
+
+            string fsbOutput = Path.Combine(CompilePath, $"{Checksum}");
+            try
+            {
+                Console.WriteLine("Compiling Audio...");
+
+                FSB fsb = new FSB();
+
+                Task drums1Stem = fsb.ConvertToMp3(KickPath, drumsKickOutput);
+                Task drums2Stem = fsb.ConvertToMp3(SnarePath, drumsSnareOutput);
+                Task drums3Stem = fsb.ConvertToMp3(CymbalsPath, drumsCymbalOutput);
+                Task drums4Stem = fsb.ConvertToMp3(TomsPath, drumsTomsOutput);
+
+                Task guitarStem = fsb.ConvertToMp3(GuitarPath, guitarOutput);
+                Task rhythmStem = fsb.ConvertToMp3(RhythmPath, rhythmOutput);
+                Task vocalsStem = fsb.ConvertToMp3(VocalsPath, vocalsOutput);
+
+                Task backingStem = fsb.MixFiles(BackingPaths, backingOutput);
+                Task crowdStem = fsb.ConvertToMp3(CrowdPath, crowdOutput);
+
+                var tasksToAwait = new List<Task> { drums1Stem, drums2Stem, drums3Stem, drums4Stem, guitarStem, rhythmStem, vocalsStem, backingStem, crowdStem };
+
+                // Await all started tasks. This ensures all conversions are completed before moving on.
+                await Task.WhenAll(tasksToAwait.ToArray());
+
+                // Create the preview audio
+                if (CustomPreview && File.Exists(PreviewPath))
+                {
+                    Task previewStem = fsb.ConvertToMp3(PreviewPath, previewOutput);
+                    await previewStem;
+                }
+                else
+                {
+                    string[] previewFiles = { drumsKickOutput, drumsSnareOutput, drumsCymbalOutput, drumsTomsOutput, guitarOutput, rhythmOutput, vocalsOutput, backingOutput };
+                    Task previewStem = fsb.MakePreview(previewFiles, previewOutput, PreviewStart, PreviewLength, FadeIn, FadeOut, PreviewVolume);
+                    await previewStem;
+                }
+                Console.WriteLine("Combining Audio...");
+                FsbList = fsb.CombineFSB4File(drumFiles, otherFiles, backingFiles, [previewOutput], fsbOutput);
+
+                Console.WriteLine("Audio Compilation Complete!");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                foreach (string file in filesToProcess)
+                {
+                    if (File.Exists(file))
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+        }
+        public static AudioCompiler CreateGh3Compiler(
+            string checksum,
+            string compilePath,
+            string saveFolder,
+            string game,
+            int previewStart,
+            int previewLength,
+            decimal previewVolume,
+            decimal fadeIn,
+            decimal fadeOut,
+            bool customPreview,
+            bool coopAudio,
+            string guitarPath,
+            string rhythmPath,
+            string[] backingPaths,
+            string crowdPath,
+            string previewPath,
+            string guitarCoopPath,
+            string rhythmCoopPath,
+            string[] backingCoopPaths
+            )
+        {
+            decimal prevStart = previewStart / 1000m;
+            decimal prevLength = previewLength / 1000m;
+            var compiler = new AudioCompiler()
+            {
+                Checksum = checksum,
+                CompilePath = compilePath,
+                SaveFolderName = saveFolder,
+                CurrentGame = game,
+                PreviewStart = prevStart,
+                PreviewLength = prevLength,
+                PreviewVolume = previewVolume,
+                FadeIn = fadeIn,
+                FadeOut = fadeOut,
+                CustomPreview = customPreview,
+                CoopAudio = coopAudio,
+                GuitarPath = guitarPath,
+                RhythmPath = rhythmPath,
+                BackingPaths = backingPaths,
+                CrowdPath = crowdPath,
+                PreviewPath = previewPath,
+                GuitarCoopPath = guitarCoopPath,
+                RhythmCoopPath = rhythmCoopPath,
+                BackingCoopPaths = backingCoopPaths,
+            };
+            return compiler;
+        }
+        public static AudioCompiler CreateGhwtCompiler(
+            string checksum,
+            string compilePath,
+            string saveFolder,
+            string game,
+            int previewStart,
+            int previewLength,
+            decimal previewVolume,
+            decimal fadeIn,
+            decimal fadeOut,
+            bool customPreview,
+            string guitarPath,
+            string rhythmPath,
+            string vocalsPath,
+            string kickPath,
+            string snarePath,
+            string cymbalsPath,
+            string tomsPath,
+            string[] backingPaths,
+            string crowdPath,
+            string previewPath
+            )
+        {
+            decimal prevStart = previewStart / 1000m;
+            decimal prevLength = previewLength / 1000m;
+            var compiler = new AudioCompiler()
+            {
+                Checksum = checksum,
+                CompilePath = compilePath,
+                SaveFolderName = saveFolder,
+                CurrentGame = game,
+                PreviewStart = prevStart,
+                PreviewLength = prevLength,
+                PreviewVolume = previewVolume,
+                FadeIn = fadeIn,
+                FadeOut = fadeOut,
+                CustomPreview = customPreview,
+                GuitarPath = guitarPath,
+                RhythmPath = rhythmPath,
+                VocalsPath = vocalsPath,
+                KickPath = kickPath,
+                SnarePath = snarePath,
+                CymbalsPath = cymbalsPath,
+                TomsPath = tomsPath,
+                BackingPaths = backingPaths,
+                CrowdPath = crowdPath,
+                PreviewPath = previewPath
+            };
+            return compiler;
         }
     }
 }
